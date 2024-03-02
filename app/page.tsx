29f2d5c728894ad574c19dabc56317a2ab7384cc
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2024-03-02 02:49:53 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import ProductCard from "./components/ProductCard";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+
+      <Link href="/users">Users page</Link>
+      <ProductCard />
+    </div>
+  );
+}
